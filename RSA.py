ea80af74551f64b1edb8f4ab73833c2c259b5f73
not a dog <assaf.treves@gmail.com>
2023-03-07 18:28:57 +0200

    replaced math.pow with pow and added comments

--- RSA.py
+++ RSA.py
@@ -1,61 +1,60 @@
 import math
-from sympy import totient as eulerFunction
 import random
-import string
+
+# returns Co-prime integer to fn, the only integer that shares a divisor of 1 with fn, which
 
 
-# returns Co-prime integer to fn, the only integer that shares a divisor of 1 with fn
 def generateE(fn):
-    e = random.randint(2,fn - 1) #Return a random int in the range(1,fn)
-    #TODO: more efficient 
-    while math.gcd(e, fn) != 1: #while the numbers have a common divider
-        e = random.randint(2,fn - 1)
+    e = random.randint(2, fn - 1)  # Return a random int in the range(1,fn)
+    # TODO: more efficient
+    while math.gcd(e, fn) != 1:  # while the numbers have a common divider
+        e = random.randint(2, fn - 1)
     return e
-#
+
+# generate a private key using q and p in the form of fn.
+# thus only the one how has q and p can generate
+
+
 def generateD(fn, e):
     k = 2
-    d = round((k * fn + 1) / e)
-    while (((k * fn + 1) % e) != 0 or d == e) :
+    d = round((k * fn + 1) / e)  # deviation in python returns a double
+    while (((k * fn + 1) % e) != 0 or d == e):
         k = k + 1
         d = round((k * fn + 1) / e)
     return d
 
 
-#
-def encrypt(plainLetter,e,mod): 
-    return math.pow(plainLetter,e) % mod 
+# takes letter,e,mod which are part of the public key and encrypts according to rsa formula
+def encrypt(plainLetter, e, mod):
+    return pow(plainLetter, e) % mod
 
-#
-def decrypt(encryptedLetter,privateKey,publicMod): 
-    return math.pow(encryptedLetter,privateKey) % publicMod
+# takes letter mod (public key) and private key and decrypts according to rsa formula
 
 
+def decrypt(encryptedLetter, privateKey, mod):
+    return pow(encryptedLetter, privateKey) % mod
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     message = str(input("Enter the message to be encrypted: "))
-    #step 1:
-    p = 2
+    # in the real world p and q are 2 large prime numbers, to save resources and time I used smaller ones
+    p = 11
     q = 7
-    #step 2:
+    # preparing the public key:
     n = p*q
-    #step 3:
     Fn = (p-1)*(q-1)
-    #step 4:
-    e = 5 
-    #generateE(Fn) #
-    d = generateD(Fn,e) # to get e the attacker needs to get p and q and that is impossible 
-    
-    publicKey = [Fn,n]
+    e = generateE(Fn)
+    # that how a public key may look like
+    publicKey = [e, n]
+    d = generateD(Fn, e)
+    # to get e the attacker needs to get p and q and that is impossible, thus d is the private key
     privateKey = d
 
+    # original message in decimal
     print(list([ord(letter) - 96 for letter in message]))
-    # for each letter I get ascii value encrypt it then convert back to ascii and add to a list  
+    # for each letter I get ascii value encrypt it and add to a list
     encryptedMessage = list([(int(encrypt((ord(letter) - 96), e, n))) for letter in message])
     print(encryptedMessage)
-    decryptedMessage = list([int(decrypt(letter,privateKey,n)) for letter in encryptedMessage])
+    # for each letter I decrypt it using the private key and add to a list
+    decryptedMessage = list([int(decrypt(letter, privateKey, n)) for letter in encryptedMessage])
     print(decryptedMessage)
-
-    temp  = list([(int(encrypt((ord(letter) - 96), e, n))) for letter in string.ascii_lowercase])
-    print(list([(ord(letter) - 96) for letter in string.ascii_lowercase]))
-    print(temp)
-    print(list([(int(decrypt(letter,privateKey,n))) for letter in temp]))
